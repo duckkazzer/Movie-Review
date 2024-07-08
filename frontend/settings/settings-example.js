6232a081eadfa37002ec46eb97a853c9ad1a8ac5
Egor Nikitin <kazzerduck@gmail.com>
2024-07-08 23:02:18 +0300

    made examples backend/.env.example and frontend/settings/settings-example.js for api and credentials

---
+++ frontend/settings/settings-example.js
@@ -0,0 +1,7 @@
+//rename file name to setting.js
+var environment = {
+    "APILink": "https://somesite.tech/api/films?order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000&page=1", //here you add your apilink
+    "APISearch": "https://somesite.tech/api/films/search-by-keyword?keyword=", //add api for search by keyword
+    "APIKey": "kissb228-ep32-404g-b00b-missg69a7efk", //add your 
+    "backendBaseUrl": "http://localhost:8000", //add initial url from backend 
+  }
